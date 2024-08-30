8efeec4b1dc9e155201e536b0ddc4cf4d4211547
Aashay30 <tamrakaraashay30@gmail.com>
2024-08-30 18:41:15 +0530

    setup file for project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Aashay Tamrakar',
+    author_email= 'tamrakaraashay30@gmail.com',
+    packages= find_packages(), # make folders as local packages where finds "__init__.py" file
+    install_requires = []
+
+)
